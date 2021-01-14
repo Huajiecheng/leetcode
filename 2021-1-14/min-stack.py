64d4966470dc29053f96fbd00f596898ea9d119b
Huajiecheng <chenghuajie1998@gmail.com>
2021-01-14 16:45:44 -0500

    2021-1-14 problems

---
+++ 2021-1-14/min-stack.py
@@ -0,0 +1,35 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.s = []
+        
+
+    def push(self, x: int) -> None:
+        if self.s:
+            self.s.append([x,min(self.s[-1][1],x)])            
+        else:
+            self.s.append([x,x])
+        
+
+    def pop(self) -> None:
+        self.s.pop()
+        
+
+    def top(self) -> int:
+        return self.s[-1][0]
+        
+
+    def getMin(self) -> int:
+        return self.s[-1][1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
